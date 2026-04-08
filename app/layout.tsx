0adfab6ae0e62b04fdca8fcd1479d20c5c7e954a
Pranav Soni <pranavsoni1101@gmail.com>
2026-04-09 02:42:46 +1000

    Added next-themes to implement light and dark modes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 export const metadata = {
   title: "GDSC ANU",
@@ -13,11 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="bg-black text-white antialiased">
-        <Navbar />
-        <main className="pt-20">{children}</main>
-        <Footer />
+        <ThemeProvider>
+          <Navbar />
+          <main className="pt-20">{children}</main>
+          <Footer />
+
+        </ThemeProvider>
       </body>
     </html>
   );
